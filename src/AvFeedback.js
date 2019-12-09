fcaed7db8860671358007eef4294af21cd706f17
lexithium <ansanchezdesign@gmail.com>
2019-12-09 12:00:01 -0500

    adding context to form feedback

--- src/AvFeedback.js
+++ src/AvFeedback.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 import { FormFeedback } from 'reactstrap';
 
 export default class AvFeedback extends Component {
-  static propTypes = Object.assign({}, FormFeedback.propTypes);
+  static propTypes = Object.assign({
+    for: PropTypes.string
+  }, FormFeedback.propTypes);
 
   static contextTypes = {
     FormCtrl: PropTypes.object.isRequired,
@@ -12,6 +14,7 @@ export default class AvFeedback extends Component {
 
   render() {
     const validation = this.context.Group.getInputState();
-    return <FormFeedback valid={!validation.error} {...this.props} />;
+    const feedback = this.context.FormCtrl.getInputState(this.props.for);
+    return <FormFeedback valid={!validation.error} {...this.props}>{feedback ? feedback.errorMessage : ''}</FormFeedback>;
   }
 }
